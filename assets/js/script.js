602449efe48c75f3e565f084fa9ea23c98ec53fc
AndrewHyland757 <hyland757@gmail.com>
2023-05-06 09:48:53 +0000

    add questions array

---
+++ assets/js/script.js
@@ -0,0 +1,98 @@
+// questions array
+const questions = [{
+    question: 'The earliest humans first appear in what epoch?',
+    choiceA: 'Oligocene',
+    choiceB: 'Paleocene',
+    choiceC: 'Miocene',
+    choiceD: 'Pliocene',
+    correct: 'C'
+},
+{
+    question: 'In most small-scale, non-industrial societies, social organization is based largely on.....?',
+    choiceA: 'Occupation',
+    choiceB: 'Education',
+    choiceC: 'Social class',
+    choiceD: 'Kinship affiliation',
+    correct: 'D'
+},
+{
+    question: "What is a ritual held at a certain point in a person's life to mark the end of one stage and the beginning of another called?",
+    choiceA: 'Quartering',
+    choiceB: 'Ceremony',
+    choiceC: 'Rite of passage',
+    choiceD: 'Sanction',
+    correct: 'C'
+},
+{
+    question: 'What is learned, and shared behaviors and beliefs?',
+    choiceA: 'Culture',
+    choiceB: 'Food',
+    choiceC: 'Language',
+    choiceD: 'Enthnicity',
+    correct: 'A'
+},
+{
+    question: 'What is the term for the process of creating a new culture by combining elements from different cultures?',
+    choiceA: 'Cultural fusion',
+    choiceB: 'Globalization',
+    choiceC: 'Cultural appropriation',
+    choiceD: 'Cultural-hybridization',
+    correct: 'D'
+},
+{
+    question: 'What animals did Jane Goodall study?',
+    choiceA: 'Gorillas',
+    choiceB: 'Chimpanzees',
+    choiceC: 'Orangutans',
+    choiceD: 'Baboons',
+    correct: 'B'
+},
+{
+    question: 'How many languages are currently spoken in the world?',
+    choiceA: 'Over 10,000',
+    choiceB: 'Around 1,000',
+    choiceC: 'Around 7,000 ',
+    choiceD: 'Around 3,000',
+    correct: 'C'
+},
+{
+    question: 'What is the revolution from hunting and gathering to food production called?',
+    choiceA: 'The Pastoral Revolution',
+    choiceB: 'The Organic Revolution',
+    choiceC: 'The Neolithic Revolution',
+    choiceD: 'The Cultivation Revolution',
+    correct: 'C'
+},
+{
+    question: 'What did Jetro Tull invent?',
+    choiceA: 'The steam engine',
+    choiceB: 'The seed drill',
+    choiceC: 'The spinning jenny',
+    choiceD: 'A rock band',
+    correct: 'B'
+},
+{
+    question: 'The Sentinelese, a famous uncontacted tribe, can be found where?',
+    choiceA: 'The Andaman Islands',
+    choiceB: 'Senegal',
+    choiceC: 'The Amazon Rainforest',
+    choiceD: 'Papa New Guinea',
+    correct: 'A'
+},
+{
+    question: 'How many bones in the human body?',
+    choiceA: '206',
+    choiceB: '106',
+    choiceC: '706',
+    choiceD: '1,006',
+    correct: 'A'
+},
+{
+    question: 'Which of these events occurred first?',
+    choiceA: 'The French Revolution',
+    choiceB: 'The Industrial Revolution',
+    choiceC: 'The American Revolution',
+    choiceD: 'The Enlightenment',
+    correct: 'D'
+}
+];
